036267fa7b86d04cb098abfd72f5d8d0eec84fdf
Joseph Freeman <freemanjj1992@gmail.com>
2021-06-15 19:06:36 -0400

    added data

---
+++ models/projects.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require ('../config/connection');
+
+class Project extends Model {}
+
+Project.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        deployed: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        gitHubLink: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        mainTech: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'projects',
+    }
+);
+
+module.exports = Project;
