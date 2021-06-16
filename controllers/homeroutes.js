2329d6daf0c6c38d556d12f0edd7ce7b490aded0
Joseph Freeman <freemanjj1992@gmail.com>
2021-06-15 21:08:42 -0400

    added new project

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
 
         const projects = projectData.map(project => project.get({ plain: true }));
         //res.status(200).json(projects);
-        res.render('projects', projects);
+        res.render('projects', {projects});
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
